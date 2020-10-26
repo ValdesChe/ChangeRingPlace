365d43efdd8d2a8cf6cbbf9e0b3d3e54d1d944f4
Valdes Che <valdesche03@gmail.com>
2020-10-26 04:48:58 +0100

    Added Fade, Scale, Rotate and translate animations

---
+++ ChangeRingPlace/ChangeRingPlace/Animations/EasingType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChangeRingPlace.Animations
+{
+    public enum EasingType
+    {
+        BounceIn,
+        BounceOut,
+        CubicIn,
+        CubicInOut,
+        CubicOut,
+        Linear,
+        SinIn,
+        SinInOut,
+        SinOut,
+        SpringIn,
+        SpringOut
+    }
+}
